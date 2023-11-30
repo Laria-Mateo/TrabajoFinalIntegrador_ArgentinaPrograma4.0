80595d05d108f2cea83655abe7500c77dd180856
Mateo Francisco Laria <mateolaria2021@gmail.com>
2023-11-30 09:14:16 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useState, useEffect } from 'react';
 
 import { useToast, Box, Flex, Input, Button, InputRightElement, InputGroup, Text, Image, ChakraProvider } from '@chakra-ui/react';
 
-
+import Header from './components/Header/header';
 import TaskList from './components/taskList/taskList';
 import TaskForm from './components/taskForm/taskForm';
 import TaskHistory from './components/taskHistory/taskHistory';
 import history from './history.jpg'
 import lista from './lista.jpg'
-import Header from './components/Header/header';
+
 
 
 function App() {
